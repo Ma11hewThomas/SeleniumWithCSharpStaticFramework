a2b038d6c6a5653d18b2a380aa0d909cc15ba31a
matthewthomasgb <matthewthomasgb@gmail.com>
2017-03-03 20:44:26 +0000

    Added capability to configure test data

---
+++ Framework/Configuration/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace Framework.Configuration
+{
+    public static class TestConfig
+    {
+        public static ConfigurationReader reader = new ConfigurationReader("WebAutomationConfiguration.config");
+
+        public static string Browser()
+        {
+            return reader.ReadValue("Browser");
+        }
+    }
+}
