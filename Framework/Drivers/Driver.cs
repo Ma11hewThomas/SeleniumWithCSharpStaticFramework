e15338bd257bde055927fb637db28f730b612e24
matthewthomasgb <matthewthomasgb@gmail.com>
2017-03-03 20:18:41 +0000

    Added driver class with basic init and close method

---
+++ Framework/Drivers/Driver.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Framework.Driver
+{
+    public class Driver
+    {
+       public static IWebDriver Instance;
+
+        public static void Initialize()
+        {
+            Instance = new ChromeDriver(@"C:\");
+        }
+
+        public static void Close()
+        {
+            Instance.Quit();
+        }
+    }
+}
