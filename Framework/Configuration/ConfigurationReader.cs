4497a9b103dc43c2720bdf009f865163aacc8a7b
matthewthomasgb <matthewthomasgb@gmail.com>
2017-03-03 20:44:26 +0000

    Added capability to configure test data

---
+++ Framework/Configuration/ConfigurationReader.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Framework.Configuration
+{
+    public class ConfigurationReader
+    {
+        XDocument xdoc;
+
+        public ConfigurationReader(string configurationFileName)
+        {
+            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+            xdoc = XDocument.Load(Path.Combine(path, configurationFileName));
+        }
+
+
+        public string ReadValue(string name)
+        {
+            string value = xdoc.Descendants("setting")
+                .Select(x => new
+                {
+                    key = x.Descendants("key").FirstOrDefault().Value,
+                    value = x.Descendants("value").FirstOrDefault().Value
+                })
+                .Where(x => x.key == name)
+                    .FirstOrDefault()
+                    .value;
+
+            return value;
+        }
+    }
+}
