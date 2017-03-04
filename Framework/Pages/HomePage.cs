c0ee0f170fecc3aa24b65099748b92b4eabcf557
matthewthomasgb <matthewthomasgb@gmail.com>
2017-03-04 09:59:27 +0000

    Added skeleton page with IsAt and GoTo

---
+++ Framework/Pages/HomePage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace Framework.Pages
+{
+    public class HomePage
+    {
+        public static bool IsAt()
+        {
+                return true;   
+        }
+
+        public static void GoTo()
+        {
+
+        }
+    }
+}
